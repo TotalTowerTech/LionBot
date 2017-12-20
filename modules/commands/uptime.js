b071b1854da2cb28742d1c80b366de9f8f09c195
Nayab <nayab.warach@gmail.com>
2017-12-20 13:17:44 -0600

    I'm running out of witty comments for these

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -4,11 +4,12 @@ module.exports.run = (client, message, args) => {
     var minutes = Math.round((uptimeData / 1000 / 60) % 60);
     var hours = Math.round((minutes / 60) % 60);
     var days = Math.round((hours / 24) % 24);
+    var outputStuffs = ('I have been up for exactly { hours } hours and { minutes } minutes. That is { days } days.')
 
     let embed = new Discord.RichEmbed()
     embed.setTitle("Uptime :clock1:")
     embed.setDescription("I mean, I'm up almost 24/7 thanks to FloppyDiskDrive, but here ya go!")
-    embed.addField(":clock1:", (`I have been up for exactly { hours } hours and { minutes } minutes. That is { days } days.`))
+    embed.addField(":clock1:", { outputStuffs })
     message.channel.send({ embed })
   }
 
