e2c2c2ea22f607412e45d2a125a14c5d73e0d88e
Nayab <nayab.warach@gmail.com>
2017-12-19 20:05:26 -0600

    GAH 2.0

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -1,14 +1,14 @@
 module.exports.run = (client, message, args) => {
     const Discord = require('discord.js')
     let uptimeData = client.uptime;
-    let minutes = Math.round((uptimeData / 1000 / 60) % 60);
-    let hours = Math.round((minutes / 60) % 60);
-    let days = Math.round((hours / 24) % 24);
+    var minutes = Math.round((uptimeData / 1000 / 60) % 60);
+    var hours = Math.round((minutes / 60) % 60);
+    var days = Math.round((hours / 24) % 24);
 
     let embed = new Discord.RichEmbed()
     embed.setTitle("Uptime :clock1:")
     embed.setDescription("I mean, I'm up almost 24/7 thanks to FloppyDiskDrive, but here ya go!")
-    embed.addField(":clock1:", (`I have been up for exactly {hours} hours and {minutes} minutes. That's {days} days.`))
+    embed.addField(":clock1:", (`I have been up for exactly {hours} hours and {minutes} minutes. That is {days} days.`))
     message.channel.send({ embed })
   }
 
