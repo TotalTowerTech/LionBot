016e15845fcd858f01f0a96ed59ea7e051fae004
Nayab <nayab.warach@gmail.com>
2017-12-19 20:01:55 -0600

    Derp: The return

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -4,12 +4,11 @@ module.exports.run = (client, message, args) => {
     let minutes = Math.round((uptimeData / 1000 / 60) % 60);
     let hours = Math.round((minutes / 60) % 60);
     let days = Math.round((hours / 24) % 24);
-    let output = `I have been up for exactly` {hours} `hours and `{minutes} `minutes. That's `{days} `days.`;
 
     let embed = new Discord.RichEmbed()
     embed.setTitle("Uptime :clock1:")
     embed.setDescription("I mean, I'm up almost 24/7 thanks to FloppyDiskDrive, but here ya go!")
-    embed.addField(":clock1:", output)
+    embed.addField(":clock1:", `I have been up for exactly {hours} hours and {minutes} minutes. That's {days} days.`)
     message.channel.send({ embed })
   }
 
