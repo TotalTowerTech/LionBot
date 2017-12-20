7355beb3e42f3511ee50d6f942fe9d7077e61eda
Nayab <nayab.warach@gmail.com>
2017-12-20 14:06:20 -0600

    wrong folder

--- embedsay.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = (client, message, args) => {
-const discord = require('discord.js');
-let phrase = args.join(" ");
-let embed = new discord.RichEmbed()
-embed.setTitle("LionBot says...")
-embed.setDescription(phrase)
-embed.setFooter(`Said by ${message.author.username} at ${new Date()}`, message.author.avatarURL)
-message.channel.send({ embed })
-}
