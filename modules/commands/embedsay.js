5857060a332beb0aeb9e3797b999ea11297ef871
Nayab <nayab.warach@gmail.com>
2017-12-20 14:07:35 -0600

    Embed Say is here!

---
+++ modules/commands/embedsay.js
@@ -0,0 +1,15 @@
+module.exports.run = (client, message, args) => {
+const discord = require('discord.js');
+let phrase = args.join(" ");
+let embed = new discord.RichEmbed()
+embed.setTitle("LionBot says...")
+embed.setDescription(phrase)
+embed.setFooter(`Said by ${message.author.username} at ${new Date()}`, message.author.avatarURL)
+message.channel.send({ embed })
+}
+module.exports.help = {
+    name: "embed say",
+    args: "[args]",
+    notes: `Repeats what you type, but attributes it to you.`,
+    category: 'fun'
+}
