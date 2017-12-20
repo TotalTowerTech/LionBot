de185fb05091fe25225bdd9d45c666311ed0d475
Nayab <nayab.warach@gmail.com>
2017-12-19 20:00:29 -0600

    Just fixing shizz

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -1,12 +1,12 @@
 module.exports.run = (client, message, args) => {
-    const discord = require('discord.js')
+    const Discord = require('discord.js')
     let uptimeData = client.uptime;
     let minutes = Math.round((uptimeData / 1000 / 60) % 60);
     let hours = Math.round((minutes / 60) % 60);
     let days = Math.round((hours / 24) % 24);
-    let output = `I have been up for exactly {hours} hours and {minutes} minutes. That's {days} days.`;
+    let output = `I have been up for exactly` {hours} `hours and `{minutes} `minutes. That's `{days} `days.`;
 
-    let embed = new discord.RichEmbed()
+    let embed = new Discord.RichEmbed()
     embed.setTitle("Uptime :clock1:")
     embed.setDescription("I mean, I'm up almost 24/7 thanks to FloppyDiskDrive, but here ya go!")
     embed.addField(":clock1:", output)
