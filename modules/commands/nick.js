31bf044fb30715413fad292d4a4e8b76ade41345
Nayab <nayab.warach@gmail.com>
2018-01-02 09:42:16 -0600

    semicolons

--- modules/commands/nick.js
+++ modules/commands/nick.js
@@ -1,6 +1,6 @@
 module.exports.run = (client, message, args, throwex) => {
   message.member.setNickname(`${args.join(" ")}`)
-  message.reply(`Ok! I set your nickname to "${args.join(" ")}." Please note that this will be blank if you cleared your nickname.`)
+  message.reply(`Ok! I set your nickname to "${args.join(" ")}". Please note that this will be blank if you cleared your nickname.`);
 }
 module.exports.help = {
     name: 'nick',
