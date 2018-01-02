b0fb42a20094707dbca6a1536b77dfd3b7ea2d05
Nayab <nayab.warach@gmail.com>
2018-01-01 19:36:25 -0600

    Debugging: Removing my idiot mistake

--- modules/commands/nick.js
+++ modules/commands/nick.js
@@ -1,11 +1,11 @@
 module.exports.run = (client, message, args, throwex) => {
   let nick = args.join(" ")
   if (nick = "") {
-  message.member.setNickname(loldoesanyonereadthese)
+  message.member.setNickname(nick)
   message.reply ("Ok! I cleared that nickname for you!")
   }
   else {
-  message.member.setNickname(loldoesanyonereadthese)
+  message.member.setNickname(nick)
   message.reply(`Ok! I set your nickname to ${args.join(" ")}`)
 }
 }
