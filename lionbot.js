c83555b720503f487fc5b730e59528ffb75c64f6
Nayab <nayab.warach@gmail.com>
2018-01-31 21:16:44 -0600

    Update bot.js

--- lionbot.js
+++ lionbot.js
@@ -1,4 +1,4 @@
-﻿/****************************************
+/****************************************
  *
  *   LionBot: (Basically Half-Baked) Moderation bot, with a few fun commands.
  *   written by ServerLion#1789
@@ -7,12 +7,12 @@
  *   it under the terms of the MIT License.
  *
  *  FloppyDiskDrive wrote the handler and helped me fix my derps. (Thanks again! c:)
- *  Jtsshieh wrote the help command and liscense file
+ *  Jtsshieh wrote the help command
  *
  *
  * *************************************/
 
-var ver = "1.0.0";
+var ver = "1.0.4";
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
@@ -21,21 +21,21 @@ const config = require("./config.json")
 var prefix = 'lion.'
 var vicCount = 0;
 var sameMsg = {};
-//Let's login to our bot account!
+var banneResponse = ["Uh Oh. Moderators Sure aren't happy", "What did you do this time?", "Why do *I* have to do the dirty work?", "Banne! banne! banne!"]
 var token = config.token
-client.login(process.env.TOKEN)
-//Set the game
+
+client.login('MzU3MzAzNDE0MzIyODg4NzE1.DVH5FA.F27e8fBnV8gAAQtu5QaxWdzn2O8')
+
 client.on('ready', () => {
     console.log("[i] LionBot " + ver + " is now ready to go!");
-    client.user.setGame("Save Net Neutrality! | lion.help");
-    //function gameRandomizer() {
-        //var presence = ["Wii Sports", `${prefix}help | LionBot v${ver}`, "stuff", "Save Net Neutrality!", `with ${client.guilds.size} guilds`];
-        //var gameSetter = presence[Math.floor(Math.random() * presence.length)];
-        //client.user.setGame(gameSetter);
-    //}
-    //var gameChooser = setInterval(gameRandomizer, 30000);
+    function gameRandomizer() {
+        var presence = ["Wii Sports", `${prefix}help | LionBot v${ver}`, "stuff", "with PrecipitationJS", `with ${client.guilds.size} guilds`];
+        var gameSetter = presence[Math.floor(Math.random() * presence.length)];
+        client.user.setGame(gameSetter);
+    }
+    var gameChooser = setInterval(gameRandomizer, 30000);
 });
-//Check for Mod Permissions
+
 var modCommand;
 
 function hasPermissions(perm) {
@@ -45,11 +45,11 @@ function hasPermissions(perm) {
         return false;
     }
 }
-//Command Stuff
-fs.readdir("./modules/commands/${f}", (err, files) => {
+
+fs.readdir(`./modules/commands/${f}`, (err, files) => {
     if (err) console.error(err);
 
-    let modules = (f => f.split(".").pop() === "js");
+    let modules = files.filter(f => f.split(".").pop() === "js");
     if (modules.length <= 0) {
         console.log("No public commands found. Running with no public commands loaded.");
         return;
@@ -57,7 +57,7 @@ fs.readdir("./modules/commands/${f}", (err, files) => {
 
     console.log(`Now loading ${modules.length} public commands.`)
     modules.forEach((f, i) => {
-        let props = require(`./modules/commands/${f}`);
+        let props = require(`/Users/nayab_warach/Documents/Discord/LionBot/LionBot/LionBot/modules/commands/${f}`);
         try {
             client.commands.set(props.help.name, props);
         } catch (err) {
@@ -69,21 +69,13 @@ fs.readdir("./modules/commands/${f}", (err, files) => {
     console.log(`Finshed loading all ${modules.length} commands.`)
 })
 
-//Errors and Responses
+
 client.on("message", message => {
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
 
 
-    if (message.content.includes(process.env.TOKEN)) {
-        let embed = new Discord.RichEmbed()
-            .setTitle("Oh no... what have you done?")
-            .setDescription("The token for LionBot has been leaked and will be leaving in 30 seconds.")
-            .setColor("RED")
-        var leave = setImmediate(message.guild.leave, 30000);
-        message.delete();
-        leave();
-    }
+
     function throwex(e) {
         var embedTitle = ["I got it... I got it... nope, I didn't get it.", "This bot is about as stable as Trump's Twitter.", "The error is a lie", "Got it! Take that, Precipitation!"];
         let embed = new Discord.RichEmbed()
@@ -93,21 +85,9 @@ client.on("message", message => {
             .setColor("RED")
         message.channel.send({ embed });
         console.log(e);
-    }
+    };
 
 
-    try {
-        let starEmote = message.reactions.equals("?");
-        if (starEmote.length < 3) {
-            message.reply("yay it work");
-        }
-    } catch (e) {
-        let embed = new Discord.RichEmbed()
-            .setTitle("An error has occured.")
-            .setDescription(`An error has occured while performing an action.\r\n${e})`)
-            .setColor("RED")
-        message.channel.send({ embed });
-    }
 
     let array = message.content.split(" ");
     let command = array[0];
@@ -121,7 +101,142 @@ client.on("message", message => {
     }
 
 });
-//Error
+client.on("guildCreate", (guild) => {
+
+    console.log(client.user.username + " was invited to and joined " + guild.name);
+    guild.channels.find(c => c.name == 'general').send("Thanks for choosing LionBot! Please run lion.help to get started. To take advantage of current and future moderation commands please create a channel called '#logs'. If your guild has over 200 members DM ServerLion#1789 and we can make an exception to that. Thanks again! -ServerLion ");
+});
+
+client.on("guildDelete", (guild) => {
+
+    console.log(client.user.username + " has left " + guild.name);
+});
+
+client.on("guildBanAdd", (guild, user, throwex) => {
+  let embed = new Discord.RichEmbed()
+
+  embed.setColor("RED")
+  embed.setTitle("Ban")
+  embed.setDescription(banneResponse[Math.floor(Math.random() * banneResponse.length)])
+  embed.addField('Member banned: ', (user))
+  if (guild.id == "336487228228370432"){
+     client.channels.get("398265731176988682").send({ embed });
+  }
+  else if (guild.id == "264445053596991498") {
+    client.channels.get("265156361791209475").send({ embed });
+
+  }
+  else{
+          guild.channels.find(c => c.name == 'logs').send({ embed });
+}
+
+
+});
+
+client.on("guildBanRemove", (guild, user, throwex) => {
+  let embed = new Discord.RichEmbed()
+
+  embed.setColor("GREEN")
+  embed.setTitle("Pardon")
+  embed.setDescription(banneResponse[Math.floor(Math.random() * banneResponse.length)])
+  embed.addField('Member unbanned:', (user))
+  if (guild.id == "336487228228370432"){
+     client.channels.get("398265731176988682").send({ embed });
+  }
+  else if (guild.id == "264445053596991498") {
+    client.channels.get("265156361791209475").send({ embed });
+
+  }
+  else{
+          guild.channels.find(c => c.name == 'logs').send({ embed });
+
+}
+});
+
+client.on("messageUpdate", (oldMessage, newMessage, guild) => {
+
+  if (oldMessage.author.bot) return;
+try{
+    let embed = new Discord.RichEmbed()
+  embed.setColor("ORANGE");
+  embed.setTitle(`:pencil2: **Message Edit**`);
+  embed.setDescription(`Message by ${oldMessage.author.username} was edited in ${oldMessage.channel}`)
+  embed.addField('Old Message', '```'+(oldMessage.content)+'```');
+  embed.addField('New Message', '```'+(newMessage.content)+'```');
+  embed.setFooter(`Edited Message by ${oldMessage.author.username} on ${new Date()}`, oldMessage.author.avatarURL)
+  if (newMessage.guild.id == "336487228228370432") {
+     client.channels.get("398333160452128778").send({ embed });
+  }
+  else if (newMessage.guild.id == "264445053596991498") {
+    client.channels.get("265156361791209475").send({ embed });
+
+  }
+  else {
+          newMessage.guild.channels.find(c => c.name == 'logs').send({ embed });
+}
+}
+catch (error){
+  console.log(error)
+}
+});
+
+client.on("messageDelete", (Message, throwex) => {
+  if (Message.author.bot) return;
+
+try{
+  let embed = new Discord.RichEmbed()
+  .setColor("RED")
+  .setTitle(":wastebasket: **Message Delete**")
+  .setDescription(`Message by ${Message.author.username} was deleted in ${Message.channel}`)
+  .addField(`Deleted Message`, '```'+(Message.content)+'```')
+  .setFooter(`Deleted Message by ${Message.author.username} on ${new Date()}`, Message.author.avatarURL)
+  if (Message.guild.id == "336487228228370432") {
+     client.channels.get("398333160452128778").send({ embed });
+  }
+  else if (Message.guild.id == "264445053596991498") {
+    client.channels.get("265156361791209475").send({ embed });
+
+  }
+  else{
+          Message.guild.channels.find(c => c.name == 'logs').send({ embed });
+
+}
+}
+catch (error){
+  console.log(error)
+}
+});
+
+client.on("guildMemberAdd", (member, throwex, user) => {
+try{
+  let embed = new Discord.RichEmbed()
+  .setColor("GREEN")
+  .setTitle("Member Join")
+  .setDescription(`Welcome to the server!`)
+  .addField(`New Member Name`, (member.user.tag))
+  .addField(`Account Created On`, (member.user.createdAt))
+  .addField(`Member Joined On`, (member.joinedAt))
+  .setThumbnail(member.user.avatarURL)
+
+  if (member.guild.id == "336487228228370432") {
+     client.channels.get("398265769567191051").send({ embed });
+  }
+  else if (member.guild.id == "264445053596991498") {
+    client.channels.get("265156361791209475").send({ embed });
+
+  }
+  else{
+          member.guild.channels.find(c => c.name == 'logs').send({ embed });
+
+}
+}
+catch (error){
+  console.log(error)
+}
+});
+
+
+
 process.on('unhandledRejection', function (err, p) {
     console.log("[X] " + err.stack);
 });
