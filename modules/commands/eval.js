f9b6e2d6bf4024b34bd23923b8f5ee7a7c373c66
Nayab <nayab.warach@gmail.com>
2018-02-02 17:00:19 -0600

    Hmm
    Code by Kaiss

--- modules/commands/eval.js
+++ modules/commands/eval.js
@@ -11,7 +11,7 @@ module.exports.run = (client, message, throwex) => {
 			message.channel.send({ embed });
 			console.log(e);
 	};
-    if(!message.author.id == 299314446428274689 || 228271067821506560) {
+    if !(message.author.id == 299314446428274689 || 228271067821506560) {
 		message.reply("No! I will not run your filthy code!");}
 	else {
 			const code = args.join(" ");
